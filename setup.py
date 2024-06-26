bda57e86d80b64bccc0291646aa82c37e675b9b6
Batuhan Taskaya <batuhan@python.org>
2024-06-25 18:27:51 -0700

    release 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="aura-sr",
-    version="0.0.1",
+    version="0.0.2",
     description="GAN-based Super-Resolution for real-world images, a variation of the GigaGAN paper.",
     py_modules=["aura_sr"],
     install_requires=[
