4491b39532a725fdfb7bfed3fface081d1a94924
Jonathan Fischoff <jonathangfischoff@gmail.com>
2024-07-19 22:04:53 +0000

    add both functions to test and requirements for testing

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from aura_sr import AuraSR
 import requests
 from io import BytesIO
 from PIL import Image
+import os
+
+os.makedirs("test", exist_ok=True)
 
 def load_image_from_url(url):
     response = requests.get(url)
@@ -9,5 +12,11 @@ def load_image_from_url(url):
     return Image.open(image_data)
 
 aura_sr = AuraSR.from_pretrained()
+
 image = load_image_from_url("https://mingukkang.github.io/GigaGAN/static/images/iguana_output.jpg").resize((256, 256))
+
+upscaled_image = aura_sr.upscale_4x(image)
+upscaled_image.save(os.path.join("test", "output.png"))
+
 upscaled_image = aura_sr.upscale_4x_overlapped(image)
+upscaled_image.save(os.path.join("test", "output_overlapped.png"))
